93ca21addd7999183f7f6d5f246f63774582ea6f
Bernard Xie <bxie94@gmail.com>
2015-07-19 23:29:05 -0700

    lots of changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     setBottomBarHeight();
+    initializeCountdown();
 });
 
 $(window).resize(function() {
@@ -8,4 +9,15 @@ $(window).resize(function() {
 
 function setBottomBarHeight() {
 	$('.bottom-bar').height(.3 * $('.bottom-image').height());
+}
+
+function initializeCountdown() {
+  $(".clock").countdown("2015/11/13", function(event) {
+      var $this = $('.clock').html(event.strftime(''
+            + '<span>%D</span> days '
+            + '<span>%H</span> hr '
+            + '<span>%M</span> min '
+            + '<span>%S</span> sec'
+          ));
+  });
 }
