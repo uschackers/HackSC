a3c73b2df0877410089bf29578489d3cf5e75141
Bernard Xie <bxie94@gmail.com>
2015-07-20 21:47:16 -0700

    removed timer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    initializeCountdown();
     setBottomBarHeight();
     entranceAnimation();
     hoverAnimation();
@@ -11,17 +10,6 @@ $(window).resize(function() {
 
 function setBottomBarHeight() {
   $('.bottom-bar').height(.3 * $('.computers-image').height());
-}
-
-function initializeCountdown() {
-  $(".clock").countdown("2015/11/13", function(event) {
-      var $this = $('.clock').html(event.strftime(''
-            + '<span>%D</span> days '
-            + '<span>%H</span> hr '
-            + '<span>%M</span> min '
-            + '<span>%S</span> sec'
-          ));
-  });
 }
 
 function entranceAnimation() {
